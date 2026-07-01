2821158857d4d30c7109bfa899bfd1f9de826dc3
Supriya Bharti <42292888+1501bharti@users.noreply.github.com>
2026-07-01 18:49:56 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+        
+        const str1 = s.split("").sort().join();
+        const str2 = t.split("").sort().join();
+
+        if (str1 === str2)
+            return true
+        else return false;
+    }
+}
